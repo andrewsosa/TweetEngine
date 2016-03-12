46dde2a9fe45751b3e09e2dd88c233c3dee2901c
andrewsosa001 <andrewsosa001@gmail.com>
2016-03-12 17:12:06 -0500

    Actually added python files now

---
+++ engine/config.py
@@ -0,0 +1,32 @@
+#
+#   Tweet Engine node configuration.
+#
+
+# Server URLS
+BASE_ADDR   = "http://localhost:8080/engine"   # Address of primary server.
+REGISTER    = BASE_ADDR + "/register"
+CONNECT     = BASE_ADDR + "/connect"
+POST        = BASE_ADDR + "/upload"
+
+
+# Logging Formats
+MESSAGE_FORMAT_FILE = '%(asctime)s %(levelname)-8s %(name)-12s %(message)s'
+MESSAGE_FORMAT_CONSOLE = '%(asctime)s %(levelname)-8s %(message)s'
+DATE_FORMAT = '%m/%d/%Y %I:%M:%S %p'
+FILE_FORMAT = '%Y-%m-%d-%I-%M-%S-%p'
+
+# Twitter API Credentials
+access_token = "472058940-Krvbmk4h58PPAwjqnhbgOwmBsIUVrJp3L1fGGq5o"
+access_token_secret = "wdPMhCxc8jj3uWkVwDAwlVTCYG6kvFJuWn2bqP08g1OCi"
+consumer_key = "DhqgE1tnvTH1KJDNPhkSkzDRZ"
+consumer_secret = "Frc9EA7PROJD366dGHj89JZPqqiqlwStK0yAqFoNnbxUsrdn9Y"
+
+twitter_creds = {
+    'access_token':access_token,
+    'access_token_secret':access_token_secret,
+    'consumer_key': consumer_key,
+    'consumer_secret':consumer_secret
+}
+
+# Node ID
+NODE_ID = "funny-parrot-94"
